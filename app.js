0d093fca20bfe9a365398635e9495b0a128b1b55
Yusufdev1998 <yusufdev1998@gmail.com>
2025-03-22 17:27:51 +0500

    READ, CREATE

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 import express from "express";
-
+const todos = [];
 const app = express();
-
+const getNextId = () => {
+  return Math.max(...todos, 0) + 1;
+};
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Bu mening birinchi serverim!");
+});
+
+app.get("/todos", (req, res) => {
+  res.send(todos);
+});
+
+app.post("/todos", (req, res) => {
+  const new_todo = req.body;
+  new_todo.id = getNextId();
+
+  todos.push(new_todo);
+
+  res.status(201).send({ msg: "Successfully created" });
 });
 
 const PORT = process.env.PORT;
